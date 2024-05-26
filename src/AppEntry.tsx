8ce45745ad2904bd491e8c2e4f7df19576652d1a
MaxensDestine <maxensdestine@gmail.com>
2024-05-25 22:11:46 -0400

    Initial design of the landing page

--- src/AppEntry.tsx
+++
@@ -1,9 +0,0 @@
-import Box from "@mui/material/Box"
-import React from "react";
-
-
-export default function AppEntry(): React.JSX.Element {
-    return <Box>
-
-    </Box>;
-}
---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,122 @@
+import React from "react";
+import Box from "@mui/material/Box"
+import Grid from '@mui/material/Unstable_Grid2';
+import IconButton from "@mui/material/IconButton/IconButton";
+import Typography from "@mui/material/Typography/Typography";
+import MenuIcon from '@mui/icons-material/Menu';
+import TuneIcon from '@mui/icons-material/Tune';
+import styled from "@mui/material/styles/styled";
+
+import imgNameBackground from "../images/me_anywhere/coffee_B&W_Cool_3.jpg";
+
+const Styles = {
+    icon: {
+        color: '#ffffff',
+        textShadow: '2px 2px grey',
+        fontSize: 36,
+    },
+
+    iconButton: {
+
+    },
+
+    background: {
+        backgroundImage: `url(${imgNameBackground})`,
+        backgroundRepeat: 'repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center top',
+        width: '100vw',
+        height: '100%',
+        minHeight: '100vh',
+        display: 'flex',
+    }
+};
+
+const RootGrid = styled(Grid)(({ theme }) => ({
+
+    [theme.breakpoints.down('sm')]: {
+        paddingInline: 18,
+        paddingTop: 28,
+        paddingBottom: 28
+    },
+    [theme.breakpoints.up('sm')]: {
+        paddingInline: 36,
+        paddingTop: 28,
+        paddingBottom: 28
+    },
+    [theme.breakpoints.up('md')]: {
+        paddingInline: 56,
+        paddingTop: 28,
+        paddingBottom: 28
+    },
+    [theme.breakpoints.up('lg')]: {
+        paddingInline: 68,
+        paddingTop: 28,
+        paddingBottom: 28
+    },
+}));
+
+const TypographyName = styled(Typography)({
+    textAlign: 'center',
+    fontFamily: 'initial',
+    fontWeight: 'bold',
+    textShadow: '0px 5px 5px dimGray, 0px 7px 7px #a3a3a3, 0px 9px 9px #a3a3a3',
+    color: 'white',
+    component: 'div',
+});
+
+const strFirstName: String = 'Maxens';
+const strLastName: String = 'Destiné';
+const strJobTitle: String = 'Software Engineer';
+const strAnd: String = '&';
+const strMuchMore: String = 'so much more';
+
+//const [state, setState] = React.useState({left: false});
+
+export default function LandingPage(props): React.JSX.Element {
+    console.log(props.theme);
+    return <Box sx={Styles.background}>
+        <RootGrid
+            container
+            direction='row'
+            justifyContent='space-between'
+            flexGrow='1'>
+            <Grid
+                container
+                direction='column'
+                justifyContent='space-between'>
+                <Grid>
+                    <IconButton sx={Styles.iconButton}>
+                        <MenuIcon sx={Styles.icon} />
+                    </IconButton>
+                </Grid>
+                <Grid>
+                    <IconButton sx={Styles.iconButton}>
+                        <TuneIcon sx={Styles.icon} />
+                    </IconButton>
+                </Grid>
+            </Grid>
+            <Grid
+                container
+                direction='column'
+                justifyContent='space-between'>
+                <Grid marginBottom={2}>
+                    <TypographyName variant='h2'>{strFirstName}</TypographyName>
+                    <TypographyName variant='h2'>{strLastName}</TypographyName>
+                </Grid>
+                <Grid sx={{ display: { xs: 'none', sm: 'none', md: 'none', lg: 'inline' } }}>
+                    <TypographyName variant='h3'>{strJobTitle}</TypographyName>
+                    <TypographyName variant='h4'>{strAnd}</TypographyName>
+                    <TypographyName variant='h4'>{strMuchMore}</TypographyName>
+                </Grid>
+                <Grid sx={{ display: { xs: 'none', sm: 'none', md: 'inline', lg: 'none' } }}>
+                    <TypographyName variant='h4'>{strJobTitle}</TypographyName>
+                    <TypographyName variant='h5'>{strAnd}</TypographyName>
+                    <TypographyName variant='h5'>{strMuchMore}</TypographyName>
+                </Grid>
+                <Grid></Grid>
+                <Grid></Grid>
+            </Grid>
+        </RootGrid>
+    </Box >;
+}
